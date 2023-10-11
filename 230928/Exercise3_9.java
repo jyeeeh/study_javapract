062e71ac73478e51bc5072f69667877e8d9cd12b
jih <jnelnl@naver.com>
2023-10-11 19:32:26 +0900

    230928 java 연습

---
+++ 230928/Exercise3_9.java
@@ -0,0 +1,12 @@
+package com.mystudy.pract;
+
+public class Exercise3_9 {
+    //변수가 영문자이거나 숫자일때만 변수 boolean 값이 true가 되도록 하기
+
+    public static void main(String[] args) {
+        char ch='z';
+        boolean b = ('a'<=ch && ch <= 'z') || ('A'<=ch && ch <= 'z') || ('0'<=ch && ch <='9'); //대문자, 소문자, 숫자일 때 구별
+
+        System.out.println(b);
+    }
+}
