2476d205bab8498c368d04070be9fe1dea8577b8
jyeeh <universe78a@gmail.com>
2023-10-18 17:41:06 +0900

    Delete

--- 230928/RoundOff.java
+++
@@ -1,9 +0,0 @@
-package com.mystudy.pract;
-
-public class Exercise3_4 {
-    //num 값중에서 백의 자리 이하 버리기
-    public static void main(String[] args) {
-        int num = 456;
-        System.out.println(num/100*100); // 나눗셈은 몫만 남음
-    }
-}
