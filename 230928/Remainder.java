44b9f2633fd156ef117d6b57b77e48a5d3b1599d
jyeeh <universe78a@gmail.com>
2023-10-18 17:40:15 +0900

    나머지 구하기

---
+++ 230928/Remainder.java
@@ -0,0 +1,8 @@
+public class Exercise3_6 {
+    /*num 값보다 크면서 가장 가까운 10의 배수에서 num 값을 뺀 나머지 구하기
+    ex) 24면 6 반환*/
+    public static void main(String[] args) {
+        int num = 24;
+        System.out.println(10-num%10);
+    }
+}
